a551667cf301eab180f995f99409df0443f8168e
Omer Gulen <omergulen@jotform.com>
2021-05-13 22:26:19 +0300

    AuthServiceGateway :: init grpc server
    Co-Authored-By: Deniz Egemen Coskun <denizegemencoskun@gmail.com>

--- src/Gateway/AuthServiceGateway.js
+++ src/Gateway/AuthServiceGateway.js
@@ -1,3 +1,11 @@
+// GRPC SETUP
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_PATH = __dirname + '../../../proto/authservice.proto';
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
+const authservice_package = grpc.loadPackageDefinition(packageDefinition).authservice_package;
+// GRPC SETUP
+
 class AuthServiceGateway {
 
   // Template code
