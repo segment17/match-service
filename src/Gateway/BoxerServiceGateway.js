4fbce242fc609d72dd34671fe1f825f3dde41fa3
Omer Gulen <omergulen@jotform.com>
2021-05-13 22:26:52 +0300

    BoxerServiceGateway :: init grpc server
    Co-Authored-By: Deniz Egemen Coskun <denizegemencoskun@gmail.com>

--- src/Gateway/BoxerServiceGateway.js
+++ src/Gateway/BoxerServiceGateway.js
@@ -1,3 +1,11 @@
+// GRPC SETUP
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_PATH = __dirname + '../../../proto/boxerservice.proto';
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
+const boxerservice_package = grpc.loadPackageDefinition(packageDefinition).boxerservice_package;
+// GRPC SETUP
+
 class BoxerServiceGateway {
 
   async getBoxer(boxerId) {
