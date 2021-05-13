609c4d4aa5ddb19ae99168eae77ff4d27ce687ef
Omer Gulen <omergulen@jotform.com>
2021-05-13 14:01:48 +0300

    AuthServiceGateway :: Use real auth service

--- src/Gateway/AuthServiceGateway.js
+++ src/Gateway/AuthServiceGateway.js
@@ -8,10 +8,31 @@ class AuthServiceGateway {
     return response;
   }
 
-  // doCallFor[function name]
-  async doCallForGetValidation(param) {
-    console.log("Real get call to AnyService with param: " + param);
-    return {}; //TODO
+  async doCallForGetValidation(obj) {
+    // Connect to Kubernetes if possible
+    if (this.client == undefined || this.client == null) {
+      if (process.env.AUTH_SERVICE_SERVICE_PORT != undefined) {
+        this.client = new authservice_package.AuthService(process.env.AUTH_SERVICE_SERVICE_HOST + ":" + process.env.AUTH_SERVICE_SERVICE_PORT, grpc.credentials.createInsecure());
+      } else {
+        this.client = new authservice_package.AuthService("0.0.0.0:50001", grpc.credentials.createInsecure());
+      }
+    }
+
+    console.log(this.client);
+
+    let response = await this.PROMISE_doCallForGetValidation(obj);
+    console.log(response);
+
+    return response;
+  }
+
+  async PROMISE_doCallForGetValidation (obj) {
+    return new Promise((resolve, reject) => {
+      this.client.Validate({token: obj}, function (err, res) {
+        console.log(res);
+        resolve(res);
+      });
+    });
   }
 
   async setupAddAdmin(obj) {
