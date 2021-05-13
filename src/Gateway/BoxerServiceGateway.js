bdcc36dcc0029fb49b75ba74071c7d498fe4b77c
Omer Gulen <omergulen@jotform.com>
2021-05-13 22:27:10 +0300

    BoxerServiceGateway :: GetBoxer method init
    Co-Authored-By: Deniz Egemen Coskun <denizegemencoskun@gmail.com>

--- src/Gateway/BoxerServiceGateway.js
+++ src/Gateway/BoxerServiceGateway.js
@@ -13,15 +13,38 @@ class BoxerServiceGateway {
     return response;
   }
 
-  async doCallForGetBoxer(param) {
-    console.log("Real get call to AnyService with param: " + param);
-    return {}; //TODO
-  }
-
   async SetupAddBoxer(obj) {
     console.log(obj);
     return null;
   }
+
+  async doCallForGetBoxer(obj) {
+    // Connect to Kubernetes if possible
+    if (this.client == undefined || this.client == null) {
+      if (process.env.BOXER_SERVICE_SERVICE_PORT != undefined) {
+        this.client = new boxerservice_package.BoxerService(process.env.BOXER_SERVICE_SERVICE_HOST + ":" + process.env.BOXER_SERVICE_SERVICE_PORT, grpc.credentials.createInsecure());
+      } else {
+        this.client = new boxerservice_package.BoxerService("0.0.0.0:50001", grpc.credentials.createInsecure());
+      }
+    }
+
+    console.log(this.client);
+
+    let response = await this.PROMISE_doCallForGetBoxer(obj);
+    console.log(response);
+
+    return response;
+  }
+
+  async PROMISE_doCallForGetBoxer (obj) {
+    return new Promise((resolve, reject) => {
+      this.client.GetBoxer({id: obj}, function (err, res) {
+        console.log(res);
+        resolve(res);
+      });
+    });
+  }
+
 
   async SetupAddBoxers(obj) {
     console.log(obj);
