bec515140cdc86d56d043ca5afe52f20019ef6ff
Omer Gulen <omergulen@jotform.com>
2021-05-13 22:25:26 +0300

    automate :: Fix, cucumber studio project ids
    Co-Authored-By: Deniz Egemen Coskun <denizegemencoskun@gmail.com>

--- automate_minikube_1.sh
+++ automate_minikube_1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 521216 --only=features
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 529997 --only=features
 kubectl delete deployments --selector=app=match-service
 kubectl delete svc --selector=app=match-service
 eval $(minikube docker-env)
@@ -12,6 +12,6 @@ while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.ty
 # kubectl exec $latest_pod -- bash -c "yarn test --tags '@EndToEnd'"
 kubectl exec $latest_pod -- bash -c "yarn test" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
 kubectl cp $latest_pod:test/results.json test/results.json
-hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 521216 --push-format cucumber-json
+hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 529997 --push-format cucumber-json
 end=$(date +%s)
 echo $(($end-$start))
--- automate_minikube_1_nopush.sh
+++ automate_minikube_1_nopush.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 521216 --only=features
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 529997 --only=features
 kubectl delete deployments --selector app=match-service
 kubectl delete svc --selector app=match-service
 eval $(minikube docker-env)
