8a829df497cb65555b9fc25bf64dc0917c8c440e
Omer Gulen <omergulen@jotform.com>
2021-05-13 14:57:52 +0300

    automate :: no need to exit twice

--- automate_minikube_1.sh
+++ automate_minikube_1.sh
@@ -10,7 +10,7 @@ kubectl apply -f manifest.yaml
 latest_pod=$(kubectl get pods --sort-by=.metadata.creationTimestamp -o jsonpath="{.items[-1].metadata.name}")
 while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for pod..." && sleep 1; done
 # kubectl exec $latest_pod -- bash -c "yarn test --tags '@EndToEnd'"
-kubectl exec $latest_pod -- bash -c "yarn test --exit" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
+kubectl exec $latest_pod -- bash -c "yarn test" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
 kubectl cp $latest_pod:test/results.json test/results.json
 hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 521216 --push-format cucumber-json
 end=$(date +%s)
