ba855a746bc2e24ca86eb4a88be4dc7db1ad5e68
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-05-19 12:34:45 +0300

    Updated automation scripts.

--- automate_local.sh
+++ automate_local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 546820 --only=features
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 546837 --only=features
 yarn test --exit
-hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 546820 --push-format cucumber-json
+hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 546837 --push-format cucumber-json
 end=$(date +%s)
 echo $(($end-$start))
--- automate_minikube_nopush.sh
+++ automate_minikube_nopush.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 546821 --only=features
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 546838 --only=features
 kubectl delete deployments --selector app=match-service
 kubectl delete svc --selector app=match-service
 eval $(minikube docker-env)
