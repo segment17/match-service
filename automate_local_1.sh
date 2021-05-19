0f41bb61fb13e7c4a35631c1d6319ea5d394c675
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2021-05-19 12:14:29 +0300

    Upadet automation scripts to v2 test runs.

--- automate_local_1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-start=$(date +%s)
-rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 529997 --only=features
-yarn test --exit
-hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 529997 --push-format cucumber-json
-end=$(date +%s)
-echo $(($end-$start))
--- automate_minikube.sh
+++ automate_minikube.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 start=$(date +%s)
 rm -rf features/cucumberstudio
-hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 529997 --only=features
-kubectl delete deployments --selector app=match-service
-kubectl delete svc --selector app=match-service
+hiptest-publisher --config-file test/hiptest-publisher.conf --test-run-id 546821 --only=features
+kubectl delete deployments --selector=app=match-service
+kubectl delete svc --selector=app=match-service
 eval $(minikube docker-env)
 docker build -t segment17hub/matchservice .
 kubectl apply -f manifest.yaml
 latest_pod=$(kubectl get pods --sort-by=.metadata.creationTimestamp -o jsonpath="{.items[-1].metadata.name}")
 while [[ $(kubectl get pods $latest_pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for pod..." && sleep 1; done
 # kubectl exec $latest_pod -- bash -c "yarn test --tags '@EndToEnd'"
-kubectl exec $latest_pod -- bash -c "yarn test --exit" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
+kubectl exec $latest_pod -- bash -c "yarn test" # Option "--exit" to exit when test run is completed. Otherwise it gets stuck in command prompt.
 kubectl cp $latest_pod:test/results.json test/results.json
-# DON'T PUSH TO CUCUMBER STUDIO
+hiptest-publisher --config-file test/hiptest-publisher.conf --push test/results.json  --test-run-id 546821 --push-format cucumber-json
 end=$(date +%s)
 echo $(($end-$start))
-kubectl exec -ti $latest_pod bash
